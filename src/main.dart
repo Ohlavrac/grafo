5b5a9662fc9e7a5e984a810846f21e1cab9f5c8f
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-05-25 11:35:02 -0300

    feat: add main function

---
+++ src/main.dart
@@ -0,0 +1,19 @@
+import 'models/graph.dart';
+import 'models/node.dart';
+
+void main() {
+  Graph grafo = Graph();
+
+  Node node1 = Node("1");
+  Node node2 = Node("2");
+  Node node3 = Node("3");
+  Node node4 = Node("4");
+
+  grafo.addNode(node1);
+  grafo.addNode(node2);
+  grafo.addNode(node3);
+  grafo.addNode(node4);
+
+  grafo.addEdge(1, node1, node2);
+  grafo.addEdge(1, node1, node1);
+}
