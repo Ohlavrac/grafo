ee087ad28d1aa4824e34000475502576a94ed5a4
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-05-25 11:34:55 -0300

    feat: add node model

---
+++ src/models/node.dart
@@ -0,0 +1,20 @@
+import 'edge.dart';
+
+class Node {
+  final String label;
+  final List<Edge> inputEdges = [];
+  final List<Edge> outputEdges = [];
+
+  Node(this.label);
+
+  void get getnode => this.label;
+
+  void addInputEdge(Edge edge) {
+    this.inputEdges.add(edge);
+  }
+
+  void addOutputEdge(Edge edge) {
+    this.outputEdges.add(edge);
+  }
+
+}
