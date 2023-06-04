ad239b1c4f5ece17b3fe4fd01d3bddd5eddba790
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-06-04 19:01:41 -0300

    feat: implements get graphs by id and get multigraphs

--- src/data/datasources/local_datasourceImpl.dart
+++ src/data/datasources/local_datasourceImpl.dart
@@ -7,4 +7,52 @@ class LocalDatasourceImpl implements LocalDatasource {
   List<Graph> getGraphs() {
     return graphs;
   }
+  
+  @override
+  Graph getGraphById(int id) {
+    return graphs[id];
+  }
+  
+  @override
+  List<Graph> getMultiGraphs() {
+    var allgraphs = graphs;
+    List<Graph> multigraphs = [];
+
+    ouderloop:
+    for (int index = 0; index < allgraphs.length; index++) {
+      for (int index2 = 0; index2 < allgraphs[index].edges.length; index2++) {
+        for (int index3 = 0; index3 < allgraphs[index].edges[index2].edges.length; index3++) {
+          for (int i = index3 + 1; i < allgraphs[index].edges[index2].edges.length; i++) {
+            if (allgraphs[index].edges[index2].edges[index3] == allgraphs[index].edges[index2].edges[i]) {
+              multigraphs.add(allgraphs[index]);
+              continue ouderloop;
+            }
+          }
+        }
+      }
+    }
+    return multigraphs;
+  }
+  
+  @override
+  List<Graph> getPseudographs() {
+    var allgraphs = graphs;
+    List<Graph> pseudographs = [];
+
+    ouderloop:
+    for (int index = 0; index < allgraphs.length; index++) {
+      for (int index2 = 0; index2 < allgraphs[index].edges.length; index2++) {
+        for (int index3 = 0; index3 < allgraphs[index].edges[index2].edges.length; index3++) {
+          if (allgraphs[index].edges[index2].edges[index3].first == allgraphs[index].edges[index2].edges[index3].last) {
+            pseudographs.add(allgraphs[index]);
+            continue ouderloop;
+          }
+        }
+      }
+    }
+
+    //TODO: CONFIRMAR SE PRECISO TER MULTIGRAFO E UM LOOP PARA SER UM PSEUDOGRAFO
+
+    return pseudographs;
+  }
 }
