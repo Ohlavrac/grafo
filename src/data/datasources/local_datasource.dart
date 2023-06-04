ec94bc28b695c464ccc2c0a226f1a56b1a8feefc
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-06-04 19:01:25 -0300

    feat: add getgraphs by id and get multigraphs

--- src/data/datasources/local_datasource.dart
+++ src/data/datasources/local_datasource.dart
@@ -2,4 +2,7 @@ import '../models/graph.dart';
 
 abstract class LocalDatasource {
   List<Graph> getGraphs();
+  Graph getGraphById(int id);
+  List<Graph> getMultiGraphs();
+  List<Graph> getPseudographs();
 }
