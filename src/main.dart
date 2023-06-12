53462dc11c80a828f236eb8e4204c6ec5fd0c658
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-06-12 09:13:18 -0300

    update

--- src/main.dart
+++ src/main.dart
@@ -5,10 +5,10 @@ import 'dart:io';
 import 'domain/usecases/get_multigraphs.dart';
 import 'domain/usecases/get_pseudograph.dart';
 import 'domain/usecases/get_vertexdegree_by_id.dart';
-import 'ui/terminal_menu.dart';
+import 'ui/show_graph.dart';
 
 void main() {
-  final TerminalMenu menu = TerminalMenu();
+  bool run = true;
 
   final GetGraphs getGraphs = GetGraphs();
   final GetGraphById getGraphById = GetGraphById();
@@ -16,48 +16,93 @@ void main() {
   final GetPseudoGraph getPseudoGraph = GetPseudoGraph();
   final GetVertexDegreeById getVertexDegreeById = GetVertexDegreeById();
   final graphs = getGraphs.getGraphs();
+  final ShowGraph showGraph = ShowGraph();
+
   String? option;
 
-  while (true) {
-    menu.drawnMenu();
-    option = stdin.readLineSync();
-    
-    if (option == "0") {
-      break;
-    } else if (option == "1") {
-      print("LISTA DE GRAFOS:");
-      print("TOTAL DE GRAFOS [${graphs.length}]");
-      for (int index = 0; index < graphs.length; index++) {
-        print("-> Grafo ID: ${graphs[index].id} | Vertices: ${graphs[index].nodes}\n");
-      }
-      print("ENTER para continuar...");
-      String? continu = stdin.readLineSync();
-      if (continu != null) {
-        continue;
-      }
-    } else if (option == "2") {
-      print("LISTA COM IDS DE MULTIGRAFOS");
-      for (int i = 0; i < getMultiGraphs.getMultiGraphs().length; i++) {
-        print("ID   ${getMultiGraphs.getMultiGraphs()[i].id} [MULTIGRAFO]");
-      }
-      print("ENTER para continuar...");
-      String? continu = stdin.readLineSync();
-      if (continu != null) {
-        continue;
-      }
-    } else if (option == "3") {
-      print(getPseudoGraph.getPseudographs());
-    } else if (option == "6") {
-      for (int i = 0; i < getVertexDegreeById.getVertexDegreeById(0).length; i++) {
-        print("VERTICE [${getGraphById.getGraphById(0).nodes[i]}] GRAU ${getVertexDegreeById.getVertexDegreeById(0)[i]}");
-      }
-      print("ENTER para continuar...");
-      String? continu = stdin.readLineSync();
-      if (continu != null) {
-        continue;
-      }
-    } else {
-      print("ERRO");
+  while (run) {
+    //menu.drawnMenu();
+    print("USE O COMANDO <help> PARA VER TODOS OS COMANDOS");
+    option = stdin.readLineSync()!.replaceAll(" ", "");
+
+    print(option);
+
+    switch (option) {
+      case "mostrargrafoporid":
+        String? graphId;
+
+        print("Informe o ID do grafo que deseja");
+        graphId = stdin.readLineSync();
+
+        showGraph.showGraphData(getGraphById.getGraphById(int.parse(graphId!)));
+        break;
+      case "mostrartodososgrafos":
+        print("LISTA DE GRAFOS:");
+        print("TOTAL DE GRAFOS [${graphs.length}]");
+        for (int index = 0; index < graphs.length; index++) {
+          print("-> Grafo ID: ${graphs[index].id} | Vertices: ${graphs[index].nodes}\n");
+        }
+        print("ENTER para continuar...");
+        String? continu = stdin.readLineSync();
+        if (continu != null) {
+          continue;
+        }
+        break;
+      case "mostrarmultigrafos":
+        print("LISTA COM IDS DE MULTIGRAFOS");
+        for (int i = 0; i < getMultiGraphs.getMultiGraphs().length; i++) {
+          showGraph.showGraphData(getMultiGraphs.getMultiGraphs()[i]);
+        }
+        print("ENTER para continuar...");
+        String? continu = stdin.readLineSync();
+        if (continu != null) {
+          continue;
+        }
+        break;
+      case "mostrarpseudografos":
+        print("LISTA COM IDS DE PSEUDOGRAFOS");
+        for (int i = 0; i < getPseudoGraph.getPseudographs().length; i++) {
+          showGraph.showGraphData(getPseudoGraph.getPseudographs()[i]);
+        }
+        print("ENTER para continuar...");
+        String? continu = stdin.readLineSync();
+        if (continu != null) {
+          continue;
+        }
+        break;
+      case "mostrargrafosdesconexos":
+        break;
+      case "mostrargrafoscompletos":
+        break;
+      case "grausdosvertices":
+        String? graphId;
+
+        print("Informe o ID do grafo que deseja");
+        graphId = stdin.readLineSync();
+
+        for (int i = 0; i < getVertexDegreeById.getVertexDegreeById(0).length; i++) {
+          print("VERTICE [${getGraphById.getGraphById(int.parse(graphId!)).nodes[i]}] GRAU ${getVertexDegreeById.getVertexDegreeById(int.parse(graphId))[i]}");
+        }
+        print("ENTER para continuar...");
+        String? continu = stdin.readLineSync();
+        if (continu != null) {
+          continue;
+        }
+        break;
+      case "help":
+        print("EXEMPLO DE COMANDOS");
+        print(">mostrar todos os grafos = Como o nome diz esse comando mostra todos os grafos carregados");
+        print(">mostrar multigrafos = Esse comando mostra os multigrafos da lista de grafos");
+        print(">mostrar pseudografos = Esse comando mostra todos os pseudografos da lista de grafos");
+        print(">mostrar grafos desconexos = Esse comando mostra todos os grafos desconexos da listra de grafos");
+        print(">mostrar grafos completos = Esse comando mostra todos os grafos complexto da lista de grafos");
+        print(">graus dos vertices = Esse comando é usado para mostrar o grau de todos os vertices de um grafo informado");
+        break;
+      case "sair":
+        run = false;
+        break;
+      default:
+        print("Comando Incorreto");
     }
   }
 }
