cdd72f4fc1c42425e1616d3e97d2d8a472fd887e
ohlavrac <joaovicrcarvalho@gmail.com>
2023-06-16 08:41:32 -0300

    feat: create show all graphs to bash

---
+++ src/commands_code/show_all_graphs.dart
@@ -0,0 +1,14 @@
+import '../domain/usecases/get_graphs.dart';
+import '../ui/terminal_menu.dart';
+
+void main() {
+  final GetGraphs getGraphs = GetGraphs();
+  final graphs = getGraphs.getGraphs();
+  final TerminalMenu menu = TerminalMenu();
+
+  print("LISTA DE GRAFOS:");
+        print("TOTAL DE GRAFOS [${graphs.length}]");
+        for (int index = 0; index < graphs.length; index++) {
+          print("-> Grafo ID: ${graphs[index].id} | Vertices: ${graphs[index].nodes}\n");
+        }
+}
