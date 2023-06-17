32ed687f300ee733adcd986dbbb8b823df0fb644
ohlavrac <joaovicrcarvalho@gmail.com>
2023-06-17 13:09:30 -0300

    refactor: show graphs

--- src/ui/show_graph.dart
+++ src/ui/show_graph.dart
@@ -4,9 +4,20 @@ class ShowGraph {
   ShowGraph();
 
 
-  void showGraphData(Graph graph) {
-    print("ID        ${graph.id} [MULTIGRAFO]");
-    print("VETICES:  ${graph.nodes}");
-    print("ARESTAS:  ${graph.edges[0].edges}");
+  void showGraphData(List<Graph> graphs) {
+
+    for (int i = 0; i < graphs.length; i++) {
+      print("ID        ${graphs[i].id}");
+      print("VETICES:  ${graphs[i].nodes}");
+      print("ARESTAS:  ${graphs[i].edges[0].edges}");
+    }
+  }
+
+  void showGraphDataMultigraph(List<Graph> graphs) {
+    for (int i = 0; i < graphs.length; i++) {
+      print("ID        ${graphs[i].id}  [MULTIGRAFO]");
+      print("VETICES:  ${graphs[i].nodes}");
+      print("ARESTAS:  ${graphs[i].edges[0].edges}");
+    }
   }
 }
